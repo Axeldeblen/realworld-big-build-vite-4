55e6b694ed03dea8e0a1457b52fe805d49ece268
Axel <67882025+Axeldeblen@users.noreply.github.com>
2022-12-13 11:35:42 +1100

    explicilty disable the source maps

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,13 @@ import { sveltekit } from '@sveltejs/kit/vite';
 
 /** @type {import('vite').UserConfig} */
 const config = {
-	plugins: [sveltekit()]
+	plugins: [sveltekit()],
+	build: {
+		sourcemap: false,
+		rollupOptions: {
+			sourcemap: false
+		}
+	}
 };
 
 export default config;
